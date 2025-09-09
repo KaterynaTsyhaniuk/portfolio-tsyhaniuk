2bb8a90bdbac127286878bac005f4dd80d531c5d
Kateryna Tsyhaniuk <kyzkakatryska@gmail.com>
2025-09-09 21:09:05 +0300

    fixed problem

--- src/js/locales.js
+++ src/js/locales.js
@@ -1,55 +1,8 @@
-// async function initI18n() {
-//   await i18next.init({
-//     lng: 'en', // мова за замовчуванням
-//     debug: true,
-//     resources: {
-//       en: {
-//         translation: await fetch('../locales/en.json').then(res => res.json()),
-//       },
-//       uk: {
-//         translation: await fetch('../locales/uk.json').then(res => res.json()),
-//       },
-//     },
-//   });
-
-//   // завантаження збереженої мови
-//   const savedLang = localStorage.getItem('lang') || 'en';
-//   await i18next.changeLanguage(savedLang);
-
-//   updateContent();
-// }
-
-// function updateContent() {
-//   document.querySelectorAll('[data-i18n]').forEach(elem => {
-//     const key = elem.getAttribute('data-i18n');
-//     elem.innerHTML = i18next.t(key);
-//   });
-// }
-
-// function changeLanguage(lang) {
-//   i18next.changeLanguage(lang, updateContent);
-//   localStorage.setItem('lang', lang);
-// }
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   document
-//     .getElementById('lang-en')
-//     ?.addEventListener('click', () => changeLanguage('en'));
-
-//   document
-//     .getElementById('lang-uk')
-//     ?.addEventListener('click', () => changeLanguage('uk'));
-// });
-
-// initI18n();
-
-// window.changeLanguage = changeLanguage;
-
 import i18next from 'i18next';
 
 async function initI18n() {
   await i18next.init({
-    lng: localStorage.getItem('lang') || 'en', // мова за замовчуванням
+    lng: localStorage.getItem('lang') || 'en',
     debug: true,
     resources: {
       en: {
@@ -67,7 +20,7 @@ async function initI18n() {
 function updateContent() {
   document.querySelectorAll('[data-i18n]').forEach(elem => {
     const key = elem.getAttribute('data-i18n');
-    elem.innerHTML = i18next.t(key); // вставляє HTML зі span-ами і стилями
+    elem.innerHTML = i18next.t(key);
   });
 }
 
